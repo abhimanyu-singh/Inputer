b68459d50618716ed083f6a6642cd0c73baf6d54
abhimanyu-singh <indivour@gmail.com>
2017-05-10 15:08:05 +0530

    Bug Fixes
    Fixed issue

--- InputerJS/inputer.js
+++ InputerJS/inputer.js
@@ -1,11 +1,16 @@
-function Inputer(inpContainer, defErrorContainer) {
-	this.inputContainer = inpContainer;
-	this.defErrorContainer = defErrorContainer;
-	this.submitButtonId;		// @TODO: Create Method to Add Submit button and assign the Submit button ID here
-	this.inputList = {};
-	this.submitButtonDependency = {};
-	this.showAlertOnIncorrectParamIfErrorContainerNotFound = false;
+function Inputer(inputerID, defErrorContainer) {
+	Inputer.instanceList[ inputerID ] = this;
 	
+	var inputContainerID		= inputerID;
+	var _defErrorContainer		= defErrorContainer;
+	var submitButtonId;			// @TODO: Create Method to Add Submit button and assign the Submit button ID here
+	var inputList				= {};
+	var submitButtonDependency	= {};
+	var outputString			= "";
+	var isOddRow				= true;
+	var showAlertOnIncorrectParamIfErrorContainerNotFound = false;
+	
+	if (typeof(_defErrorContainer)	=== 'undefined') _defErrorContainer	= "";
 	
 	this.selectOption = function(inpID, options, selected){
 		var selectOp = "<select id='" + inpID + "'>\n";
@@ -15,33 +20,38 @@ function Inputer(inpContainer, defErrorContainer) {
 			selectOp += ">" + options[i].Display + "</option>\n";
 		}
 		selectOp += "</select>\n";
-		document.getElementById(this.inputContainer).innerHTML += selectOp;
+		outputString += selectOp;
 		return selectOp;
 	}
-	
 	
 	this.createInputWithBR = function(inpId, inpType, defValue, submitButtonId, isManadatory, errorMsgContainerId, style, className, otherOptions) {
 		this.createInput(inpId, inpType, defValue, submitButtonId, isManadatory, errorMsgContainerId, style, className, otherOptions);
 		this.addLineBreak();
 	}
 	
-	
-	this.createInputWithLabel = function(display, inpId, inpType, defValue, submitButtonId, isManadatory, errorMsgContainerId, style, className, otherOptions) {
-		document.getElementById(this.inputContainer).innerHTML += "<div class='inputerLabelContainer'>" + display + "</div>";
-//		document.getElementById(this.inputContainer).innerHTML += ""<div class='inputerValueDivContainer'>";
+	this.createInputWithLabel = function(displayName, inpId, inpType, defValue, submitButtonId, isManadatory, errorMsgContainerId, style, className, otherOptions) {
+		var bgColorClass = "evenRowClass";
+		if(isOddRow) bgColorClass = "oddRowClass";
+		isOddRow = !isOddRow;
+		
+		outputString += "<div class='inputerDivContainer " + bgColorClass + "'>";
+		outputString += "<div class='inputerLabelContainer " + bgColorClass + "'>" + displayName + "</div>";
+		
 		this.createInput(inpId, inpType, defValue, submitButtonId, isManadatory, errorMsgContainerId, style, className, otherOptions);
-//		document.getElementById(this.inputContainer).innerHTML += "</div>";
+		// inputList[inpId].displayName = displayName;
+		
+		outputString += "</div>";
 		this.addLineBreak();
 	}
-	
 	
 	this.createInput = function(inpId, inpType, defValue, submitButtonId, isManadatory, errorMsgContainerId, style, className, otherOptions) {
 		//	this.submitButtonDependency[submitButtonId][inpId] = false;
 		// Provide Default Values for Last few Params
-		if (typeof(isManadatory)		==='undefined') isManadatory		= true;
-		if (typeof(className)			==='undefined') className			= "";
-		if (typeof(errorMsgContainerId)	==='undefined') errorMsgContainerId	= this.defErrorContainer;
-		if (typeof(otherOptions)		==='undefined') otherOptions		= "";
+		if (typeof(isManadatory)		=== 'undefined') isManadatory			= true;
+		if (typeof(className)			=== 'undefined') className				= "inputBox";
+		if (typeof(errorMsgContainerId)	=== 'undefined') errorMsgContainerId	= _defErrorContainer;
+		if (typeof(otherOptions)		=== 'undefined') otherOptions			= "";
+		if (typeof(style)				=== 'undefined') style					= "";
 		
 		inpType = inpType.toLowerCase();							// Convert to Lower Case for Easy Comparison
 		var inlineType = "text";
@@ -51,7 +61,7 @@ function Inputer(inpContainer, defErrorContainer) {
 		if(inpType == "bool")	{
 			if(defValue == "true"	|| defValue == true	|| defValue == 1) defValue = "1";
 			if(defValue == "false"	|| defValue == false|| defValue == 0) defValue = "0";
-			return this.selectOption(inpId, [{Display:"Yes", Value:"1"},{Display:"No", Value:"1"}], defValue);	// Select Bool
+			return this.selectOption(inpId, [{Display:"Yes", Value:"1"},{Display:"No", Value:"0"}], defValue);	// Select Bool
 		}
 		
 		if(inpType == "date")	placeholder= "dd-mm-yyyy";
@@ -64,20 +74,22 @@ function Inputer(inpContainer, defErrorContainer) {
 			inlineType = inpType;	// These Types will define Input box behavior as well
 		
 		var inpStr = "\n<input ";
-		inpStr += "class='"	+ className	+ "' ";
+		
+		inpStr += "class='"	+ className + "' ";
 		inpStr += "id='"	+ inpId		+ "' ";
 		inpStr += "type='"	+ inlineType+ "' ";
 		inpStr += "style='"	+ style+ "' ";
 		inpStr += otherOptions;
 		inpStr += "value='"	+ defValue	+ "' ";
 		inpStr += "placeholder= '"	+ placeholder + "' ";
-		inpStr += "onblur=this.verifyInputValue('" + inpId + "') ";
+		inpStr += "onblur=\"Inputer.verifyInputValue('" + inputContainerID + "', '" + inpId + "')\" ";
 		inpStr += "/>\n";
 		
 		if(inpType == "date")	{
 			inpStr += "<script>$( function() {$( \"#" + inpId + "\" ).datepicker();} );</script>\n";
 		}
 		var inpInfo = {
+			displayName	: inpId,
 			type		: inpType,
 			value		: defValue,
 			submitButton: submitButtonId,
@@ -85,21 +97,20 @@ function Inputer(inpContainer, defErrorContainer) {
 			errContainer: errorMsgContainerId
 		};
 		
-		this.inputList[inpId] = inpInfo;	// Save the required Info
+		inputList[inpId] = inpInfo;	// Save the required Info
 		
-		document.getElementById(this.inputContainer).innerHTML += inpStr;
+		outputString += inpStr;
 		return inpStr;
 	}
-	
 	
 	/************************************************************
 	 * Method: verifyInputValue is called whenever an Inputer
 	 * input looses Focus. It then validates the input value.
 	 ************************************************************/
 	
-	this.verifyInputValue = function(inpID) {
+	this.verifyInputValue = function(inpID)	{
 		var curVal		= document.getElementById(inpID).value;
-		var inp			= this.inputList[inpID];
+		var inp			= inputList[inpID];
 		var validated	= true;
 		var errorStr	= "Incorrect Value";
 		
@@ -137,25 +148,39 @@ function Inputer(inpContainer, defErrorContainer) {
 		
 		if(inp.isMandatory && curVal == ""){
 			validated	= false;
-			errorStr	= inpID +" is Mandatory and Cannot be Left Blank.";
+			errorStr	= inputList[inpID].displayName +" is Mandatory and Cannot be Left Blank.";
 		}
 		
 		if(! validated) {
-			document.getElementById( inpID ).style.backgroundColor = '#F4DEDE';
+			document.getElementById( inpID ).className = 'inputBoxInvalid';
+			// document.getElementById( inpID ).style.backgroundColor = '#F4DEDE';
 			if(inp.errContainer != "")
 				document.getElementById( inp.errContainer ).innerHTML = "<span style='color:red;'>"+errorStr+"</span>";
 			else if (this.showAlertOnIncorrectParamIfErrorContainerNotFound)
 				alert (errorStr);
 		}
 		else {
-			document.getElementById( inpID ).style.backgroundColor = '';
+			// document.getElementById( inpID ).style.backgroundColor = '';
+			document.getElementById( inpID ).className = 'inputBox';
 			if(inp.errContainer != "")
 				document.getElementById( inp.errContainer ).innerHTML = '';
 		}
 	}
-	
-	this.addLineBreak = function(){
-		document.getElementById(this.inputContainer).innerHTML += "</br>";
+	this.addLineBreak	= function()		{
+		outputString += "</br>";
 	}
-	
+	this.addToStream	= function(str)		{
+		outputString += str;
+	}
+	this.render			= function()	{
+		document.getElementById(inputContainerID).innerHTML = outputString;
+	}
+}
+
+Inputer.instanceList	= {};
+Inputer.getInstanceById	= function(inpID)	{
+	return Inputer.instanceList[inpID];
+}
+Inputer.verifyInputValue = function(inputerID, inpID)	{
+	Inputer.getInstanceById(inputerID).verifyInputValue(inpID);
 }
